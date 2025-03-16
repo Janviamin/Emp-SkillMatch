57f64351dc303f0930cda9017a11fc6d29d43a27
Janvi <75463037+Janviamin@users.noreply.github.com>
2025-03-16 13:33:43 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,6 @@
 import pandas as pd
 import streamlit as st
 
-# Streamlit UI
-st.title("Employee Matching System")
-
-uploaded_file = st.file_uploader("Upload Project Requirement File", type=["xlsx"])
-if uploaded_file:
-    st.write("File uploaded successfully!")
-
 # Load employee database (this acts as a backend database)
 @st.cache_data
 def load_employee_data():
